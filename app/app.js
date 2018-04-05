be52d21dd3b55f5ff1b6ef2d04bae3badd9e82b5
bao <baoyeyene@gmail.com>
2018-04-05 14:57:44 +0800

    1. update server 2. update mina

--- app/app.js
+++ app/app.js
@@ -1,29 +1,84 @@
 const express = require('express');
 const superagent = require('superagent');
+const http = require('http');
 
 const app = express();
 
+function getJson (options, onResult) {
+    var port = options.port == 443 ? https : http;
+    return new Promise(function (resolve, reject) {
+        var req = port.request(options, function (res) {
+            var output = '';
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                output += chunk;
+            });
+            res.on('end', function () {
+                resolve(JSON.parse(output));
+            });
+        });
+        req.on('error', function (err) {
+            reject();
+        });
+        req.end();
+    });
+};
+
+const getWeather = (city) => {
+    return getJson({
+        host: 'weixin.jirengu.com',
+        port: 80,
+        path: '/weather?key=study_javascript_in_jirengu.com&location=' + city,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+const getHourly = (city) => {
+    return getJson({
+        host: 'weixin.jirengu.com',
+        port: 80,
+        path: '/weather/future24h?key=study_javascript_in_jirengu.com&location=' + city,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+app.get('/all', function (req, res) {
+    const city = req.query.city || 'beijing';
+    Promise.all([getWeather(city), getHourly(city)]).then(data => {
+        res.send({
+            data: data[0].weather[0],
+            hourly: data[1].hourly
+        });
+    });
+});
+
 app.get('/', function (req, res) {
-  res.set('Content-type', 'application/json;utf-8');
-  const city = req.query.city || 'beijing';
-  const url = 'http://weixin.jirengu.com/weather?key=study_javascript_in_jirengu.com&location=' + encodeURIComponent(city)
-  const sreq = superagent.get(url);
-  sreq.pipe(res);
-  sreq.on('end', function(){
-  });
+    res.set('Content-type', 'application/json;utf-8');
+    const city = req.query.city || 'beijing';
+    const url = 'http://weixin.jirengu.com/weather?key=study_javascript_in_jirengu.com&location=' + encodeURIComponent(city)
+    const sreq = superagent.get(url);
+    console.log('sreq: ', sreq);
+    sreq.pipe(res);
+    sreq.on('end', function () {
+    });
 });
 
 app.get('/hourly', function (req, res) {
-  res.set('Content-type', 'application/json;utf-8');
-  const city = req.query.city || 'beijing';
-  const url = 'http://weixin.jirengu.com/weather/future24h?key=study_javascript_in_jirengu.com&location=' + encodeURIComponent(city)
-  const sreq = superagent.get(url);
-  sreq.pipe(res);
-  sreq.on('end', function(){
-  });
+    res.set('Content-type', 'application/json;utf-8');
+    const city = req.query.city || 'beijing';
+    const url = 'http://weixin.jirengu.com/weather/future24h?key=study_javascript_in_jirengu.com&location=' + encodeURIComponent(city)
+    const sreq = superagent.get(url);
+    sreq.pipe(res);
+    sreq.on('end', function () {
+    });
 });
 
 app.listen(3000, function () {
-  console.log('http://localhost:3000');
+    console.log('http://localhost:3000');
 });
-
--- mina/pages/index/index.js
+++ mina/pages/index/index.js
@@ -1,96 +1,129 @@
 //index.js
 //获取应用实例
 var QQMapWX = require('../../lib/qqmap-wx-jssdk.js');
-const app = getApp()
+const app = getApp();
 
 Page({
-  data: {
-      todayHourly: [],
-      city: {},
-      weather: {}
-  },
-  goto: function (data) {
-    console.log('data: ', data);
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onPullDownRefresh: function () {
-    console.log('refreshing...');
-    this.getWeather();
-    this.getHourlyWeather();
-    wx.stopPullDownRefresh()
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    var that = this;
-    // 实例化API核心类
-    var qqmapsdk = new QQMapWX({
-      key: 'RVSBZ-ETTHF-DU7JS-N4L73-C5F7V-UHFN6'
-    });
-    wx.getLocation({
-      type: 'wgs84',
-      success: function (res) {
-        //2、根据坐标获取当前位置名称，显示在顶部:腾讯地图逆地址解析
-        qqmapsdk.reverseGeocoder({
-          location: {
-            latitude: res.latitude,
-            longitude: res.longitude
-          },
-          success: function (addressRes) {
-            var address = addressRes.result.address_component;
-            that.setData({
-                city: address.city
+    data: {
+        todayHourly: [],
+        city: '',
+        weather: {},
+        loading: true,
+        cityName: '',
+        opacity: 0,
+        showIn: ''
+    },
+    onPullDownRefresh: function () {
+        console.log('refreshing...', this.city);
+        if (this.city) {
+            this.getWeather(this.city);
+            this.getHourlyWeather(this.city);
+        }        
+        wx.stopPullDownRefresh()
+    },
+    scroll: function (e) {
+        console.log('scrolling...', e);
+        
+    },
+    scrollToTop: function(e) {
+        console.log('top: ', e);
+    },
+    //事件处理函数
+    bindViewTap: function () {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+    onLoad: function () {
+        this.getLocation((city) => {
+            this.getWeather(city);
+            this.getHourlyWeather(city);
+        });
+    },
+    getLocation: function (callback) {
+        var self = this;
+        var qqmapsdk = new QQMapWX({
+            key: 'RVSBZ-ETTHF-DU7JS-N4L73-C5F7V-UHFN6'
+        });
+        wx.getLocation({
+            type: 'wgs84',
+            success: function (res) {
+                //2、根据坐标获取当前位置名称，显示在顶部:腾讯地图逆地址解析
+                qqmapsdk.reverseGeocoder({
+                    location: {
+                        latitude: res.latitude,
+                        longitude: res.longitude
+                    },
+                    success: function (addressRes) {
+                        var address = addressRes.result.address_component.city;
+                        address = 'wuhan';
+                        callback(address);
+                        self.city = address;
+                        self.setData({
+                            city: address
+                        });
+                    }
+                })
+            }
+        });
+    },
+    getHourlyWeather: function (city) {
+        if (!city) return;
+        var self = this;
+        if (!this.loading) {
+            this.setData({
+                loading: true
             })
-          }
+        }
+        wx.request({
+            url: 'https://weather.sanbf.cn/hourly?city=' + city,
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function (res) {
+                self.logs = res.data.hourly;
+                self.todayHourly = res.data.hourly;
+                self.setData({
+                    todayHourly: res.data.hourly,
+                    loading: false,
+                    opacity: 1,
+                    showIn: 'show-in'
+                });
+            }
+        });
+    },
+    getWeather: function (city) {
+        if (!city) return;
+        var slef = this;
+        if (!this.loading) {
+            this.setData({
+                loading: true
+            })
+        }
+        wx.request({
+            url: 'https://weather.sanbf.cn?city=' + city,
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function (res) {
+                const data = res.data.weather[0];
+                slef.setData({
+                    weather: {
+                        now: data.now,
+                        air: data.now.air_quality.city,
+                        future: data.future,
+                        suggestion: data.today.suggestion,
+                        loading: false,
+                        opacity: 1
+                    }
+                });
+            }
         })
-      }
-    })
-  },
-  getHourlyWeather: function () {
-    var self = this;
-    wx.request({
-        url: 'https://weixin.jirengu.com/weather/future24h?location=%E5%8C%97%E4%BA%AC%E5%B8%82&key=study_javascript_in_jirengu.com', //仅为示例，并非真实的接口地址
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-        self.logs = res.data.hourly;
-        self.todayHourly = res.data.hourly;
-        self.setData({
-            todayHourly: res.data.hourly
-        })
-      }
-    })
-  },
-  getWeather: function () {
-    var that = this;
-    wx.request({
-        url: 'https://weixin.jirengu.com/weather?location=%E5%8C%97%E4%BA%AC%E5%B8%82&key=study_javascript_in_jirengu.com', //仅为示例，并非真实的接口地址
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-          const data = res.data.weather[0];
-          that.setData({
-              weather: {
-                  now: data.now,
-                  air: data.now.air_quality.city,
-                  future: data.future,
-                  suggestion: data.today.suggestion
-              }
-          })
-      }
-    })
-  },
-  
-  onShow: function () {
-      this.getWeather();
-      this.getHourlyWeather();
-  }
+    },
+    onShow: function () {
+        // this.getLocation((city) => {
+        //     this.getWeather(city);
+        //     this.getHourlyWeather(city);
+        // });
+    }
 })
